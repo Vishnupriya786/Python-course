b0ccf4ec86bb6cea74f06ff16a109ded96349b65
Vishnupriya Karunakaran <vishnupriya.k@thoughtworks.com>
2025-04-06 22:14:17 +0530

    Add code for operators

---
+++ Operators/Operators.py
@@ -0,0 +1,24 @@
+#Addition
+print("1 + 1 = ", 1 + 1)
+
+#Subtaction
+print("5 - 1 = ", 5 - 1)
+
+#Multiplication
+print("5 * 10 = ", 5 * 10)
+
+#Division - output in decimal
+print("4 / 2 = ", 4 / 2)
+
+#Floor division - output in integer
+print("4 // 2 = ", 4 // 2)
+
+#Modulus - outputs reminder
+print("4 % 2 = ", 4 % 2)
+
+#exponent
+print("4 ** 2 = ", 4 ** 2)
+
+#BODMAS
+print("(4 + 2) * 10 / 5 = ",(4 + 2) * 10 / 5)
+print("(4 + 2) * 10 // 5 = ",(4 + 2) * 10 // 5)
